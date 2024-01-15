95a8182cf7c3422ca0878da0a2ce2fd90c1c829d
Emanuel Berger <emanuelberger49@gmail.com>
2024-01-15 10:42:29 +0100

    start editing

---
+++ 3_Kartenspiel/Kartenspiel/spiel.java
@@ -0,0 +1,10 @@
+public class spiel {
+    private karte karte1;
+    private karte karte2;
+    private karte karte3;
+    private int punkteSpieler;
+
+    public spiel() {
+        // Konstruktor
+    }
+}
