2c65032fda0b429851cb9e0fa115d87e25a6b0fa
Emanuel Berger <emanuelberger49@gmail.com>
2024-01-17 12:49:59 +0100

    amogus

--- 3_Kartenspiel/Kartenspiel/spiel.java
+++ 3_Kartenspiel/Kartenspiel/spiel.java
@@ -1,10 +1,12 @@
 public class spiel {
-    private karte karte1;
-    private karte karte2;
-    private karte karte3;
+    private karte karte1; // Vorder- und Rückseite mit Baum
+    private karte karte2; // Vorderseite Baum, Rückseite leer
+    private karte karte3; // Vorderseite und Rückseite leer
     private int punkteSpieler;
 
     public spiel() {
-        // Konstruktor
+        karte1 = new karte("Baum", "Baum");
+        karte2 = new karte("Baum", "leer");
+        karte3 = new karte("leer", "leer");
     }
 }
