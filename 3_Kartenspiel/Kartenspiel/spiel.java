226976c0349541b4399520953cedc76b33cd8a5a
Emanuel Berger <emanuelberger49@gmail.com>
2024-01-17 12:55:07 +0100

    first game

--- 3_Kartenspiel/Kartenspiel/spiel.java
+++ 3_Kartenspiel/Kartenspiel/spiel.java
@@ -3,10 +3,38 @@ public class spiel {
     private karte karte2; // Vorderseite Baum, Rückseite leer
     private karte karte3; // Vorderseite und Rückseite leer
     private int punkteSpieler;
+    private int punkteDealer;
 
     public spiel() {
+        createCards();
+    }
+    
+    public void createCards() {
         karte1 = new karte("Baum", "Baum");
         karte2 = new karte("Baum", "leer");
         karte3 = new karte("leer", "leer");
+
     }
+
+    public void countCards(int count) {
+        for(int i=0;i<count;i++) {
+            createCards();
+            if(karte1.getVorderseite() == karte1.getRückseite()) {
+                punkteDealer += 1;
+            } else{
+                punkteSpieler += 1;
+            }            
+            if(karte2.getVorderseite() == karte2.getRückseite()) {
+                punkteDealer += 1;
+            } else{
+                punkteSpieler += 1;
+            }            
+            if(karte3.getVorderseite() == karte3.getRückseite()) {
+                punkteDealer += 1;
+            } else{
+                punkteSpieler += 1;
+            }
+        }
+    }
+
 }
