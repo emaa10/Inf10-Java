938372cc566070e0018801a8b312cfc37b48d4b5
Emanuel Berger <emanuelberger49@gmail.com>
2024-01-24 13:28:03 +0100

    lampe code

--- 4_Ampel/Ampel/Ampel.java
+++ 4_Ampel/Ampel/Ampel.java
@@ -1,6 +1,10 @@
 
 public class Ampel
 {
+    private Lampe lampeOben;
+    private Lampe lampeMitte;
+    private Lampe lampeUnten;
+
     public String farbe = "Rot";
     public Ampel(String newFarbe)
     {
---
+++ 4_Ampel/Ampel/Lampe.java
@@ -0,0 +1,18 @@
+
+public class Lampe
+{
+    
+    public String farbe;
+    
+    public Lampe(String newFarbe) {
+        farbe = newFarbe;
+    }
+
+
+    public void farbeSetzen(String newFarbe) {
+        farbe = newFarbe;
+    }
+    public String farbeGeben() {
+        return farbe;
+    }
+}
