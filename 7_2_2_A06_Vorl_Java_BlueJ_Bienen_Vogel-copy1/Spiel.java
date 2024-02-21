1e99ffd41a3e27ebee827451039e59b03340a6cd
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2024-02-21 11:51:54 +0000

    coding some shit in school because school is shit

--- 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
+++ 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
@@ -13,8 +13,17 @@ public class Spiel extends Ereignisbehandlung
   
   
   public Spiel(){
-      berg1=new Berge();
-      insekt1=new Insekt(1);
+    berg1 = new Berge();
+    insekt1 = new Insekt(1);
+    himmel1 = new Himmel();
+    wiese1 = new Wiese();
+    vogel1 = new Vogel();
+    berg2 = new Berge();
+    insekt2 = new Insekt(1);
+    berg1.PositionSetzen(101, 315);
+    insekt1.PositionSetzen(435, 192);
+    berg2.PositionSetzen(533, 400);
+    insekt2.PositionSetzen(637, 221);
       
   }
   @Override
