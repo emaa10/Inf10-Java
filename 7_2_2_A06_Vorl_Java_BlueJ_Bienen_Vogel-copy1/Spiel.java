f67d9b7c0de589aee1ebe713561236ec82537665
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-04 08:20:58 +0100

    sorting

--- 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
+++ 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
@@ -1,14 +1,17 @@
 public class Spiel extends Ereignisbehandlung
 {
   private Berge berg1;
+  private Berge berg2;
+  private Berge berg3;
+
   private Insekt insekt1;
+  private Insekt insekt2;
+  private Insekt insekt3;
+
   private Himmel himmel1;
   private Wiese wiese1;
   private Vogel vogel1;
-  private Berge berg2;
-  private Berge berg3;
-  private Insekt insekt2;
-  private Insekt insekt3;
+
   private char taste;
   public Text gameover;
   public Text restart;
@@ -18,15 +21,18 @@ public class Spiel extends Ereignisbehandlung
   
   
   public Spiel(){
-    himmel1 = new Himmel();
-    wiese1 = new Wiese();
     berg1 = new Berge();
-    insekt1 = new Insekt(1);
-    vogel1 = new Vogel();
     berg2 = new Berge();
     berg3 = new Berge();
+    
+    insekt1 = new Insekt(1);
     insekt2 = new Insekt(1);
     insekt3 = new Insekt(1);
+
+    himmel1 = new Himmel();
+    wiese1 = new Wiese();
+    vogel1 = new Vogel();
+
     gameover = new Text();
     restart = new Text();
     punkteText = new Text();
@@ -38,9 +44,10 @@ public class Spiel extends Ereignisbehandlung
     gameover.SichtbarkeitSetzen(false);
     restart.SichtbarkeitSetzen(false);
     berg1.PositionSetzen(270, 315);
-    insekt1.PositionSetzen(300, 192);
     berg2.PositionSetzen(700, 315);
     berg3.PositionSetzen(500, 315);
+
+    insekt1.PositionSetzen(300, 192);
     insekt2.PositionSetzen(637, 50);
     insekt3.PositionSetzen(50, 50);
     wiese1.NachHintenBringen();
@@ -54,27 +61,26 @@ public class Spiel extends Ereignisbehandlung
     System.out.println(taste);
     if(taste == 'r') {
       berg1.PositionSetzen(270, 315);
-      insekt1.PositionSetzen(300, 192);
       berg2.PositionSetzen(700, 315);
       berg3.PositionSetzen(500, 315);
+      insekt1.PositionSetzen(300, 192);
       insekt2.PositionSetzen(637, 50);
-    insekt3.PositionSetzen(50, 50);
+      insekt3.PositionSetzen(50, 50);
       vogel1.PositionSetzen(93, 212);
       gameover.SichtbarkeitSetzen(false);
       restart.SichtbarkeitSetzen(false);
       Zeichenfenster.TaktgeberStarten();
-
     }
   }
 
   @Override
   public void TaktImpulsAusführen(){
       berg1.Bewegen();
-      insekt1.Bewegen();
       berg2.Bewegen();
+      berg3.Bewegen();
+      insekt1.Bewegen();
       insekt2.Bewegen();
       insekt3.Bewegen();
-      berg3.Bewegen();
 
       punkteText.TextSetzen(String.valueOf(punkte));
 
