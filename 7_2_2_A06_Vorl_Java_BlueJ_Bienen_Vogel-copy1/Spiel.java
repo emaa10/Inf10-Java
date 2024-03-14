68e3354e3ac60a888add0c2e44481a036ae81738
Emanuel Berger <emanuelberger49@gmail.com>
2024-03-14 16:35:59 +0100

    berg array

--- 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
+++ 7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Spiel.java
@@ -4,10 +4,7 @@ public class Spiel extends Ereignisbehandlung
   private Wiese wiese1;
   private Vogel vogel1;
 
-  private Berge berg1;
-  private Berge berg2;
-  private Berge berg3;
-
+  Berge[] berge;
   Insekt[] insekten;
 
   public Text gameover;
@@ -23,9 +20,10 @@ public class Spiel extends Ereignisbehandlung
     wiese1 = new Wiese();
     vogel1 = new Vogel();
 
-    berg1 = new Berge();
-    berg2 = new Berge();
-    berg3 = new Berge();
+    berge = new Berge[2];
+    for(int i = 0; i<2; i++) {
+      berge[i] = new Berge();
+    }
     
 
     insekten = new Insekt[3];
@@ -43,9 +41,8 @@ public class Spiel extends Ereignisbehandlung
     punkteText.FarbeSetzen("gelb");
     gameover.SichtbarkeitSetzen(false);
     restart.SichtbarkeitSetzen(false);
-    berg1.PositionSetzen(270, 315);
-    berg2.PositionSetzen(700, 315);
-    berg3.PositionSetzen(500, 315);
+    berge[0].PositionSetzen(270, 315);
+    berge[1].PositionSetzen(700, 315);
 
     insekten[0].PositionSetzen(300, 192);
     insekten[1].PositionSetzen(637, 50);
@@ -59,9 +56,8 @@ public class Spiel extends Ereignisbehandlung
   public void TasteGedrückt (char taste) {
     System.out.println(taste);
     if(taste == 'r' && taktgeberStatus == false) {
-      berg1.PositionSetzen(270, 315);
-      berg2.PositionSetzen(700, 315);
-      berg3.PositionSetzen(500, 315);
+      berge[0].PositionSetzen(270, 315);
+      berge[1].PositionSetzen(700, 315);
       insekten[0].PositionSetzen(300, 192);
       insekten[1].PositionSetzen(637, 50);
       insekten[2].PositionSetzen(50, 50);
@@ -75,9 +71,8 @@ public class Spiel extends Ereignisbehandlung
   @Override
   public void TaktImpulsAusführen(){
       taktgeberStatus = true;
-      berg1.Bewegen();
-      berg2.Bewegen();
-      berg3.Bewegen();
+      berge[0].Bewegen();
+      berge[1].Bewegen();
       for(Insekt ins :insekten) { // geht oben nicht weil da im feld noch nix drin war
         ins.Bewegen();
       }
@@ -90,24 +85,26 @@ public class Spiel extends Ereignisbehandlung
       }
 
       // berg logic
-      if(berg1.Berührt(vogel1) || berg2.Berührt(vogel1) || berg3.Berührt(vogel1)) { 
-        punkte = 0;
-
-        gameover.TextSetzen("GAME OVER");
-        gameover.GanzNachVornBringen();
-        gameover.SichtbarkeitSetzen(true);
-        gameover.TextGrößeSetzen(100);
-        gameover.PositionSetzen(125, 250);
-        gameover.FarbeSetzen("rot");
-
-        restart.TextSetzen("press r to restart");
-        restart.GanzNachVornBringen();
-        restart.SichtbarkeitSetzen(true);
-        restart.TextGrößeSetzen(50);
-        restart.PositionSetzen(200, 300);
-
-        taktgeberStatus = false;
-        Zeichenfenster.TaktgeberStoppen();
+      for(Berge berg :berge) {
+        if(berg.Berührt(vogel1)) { 
+          punkte = 0;
+  
+          gameover.TextSetzen("GAME OVER");
+          gameover.GanzNachVornBringen();
+          gameover.SichtbarkeitSetzen(true);
+          gameover.TextGrößeSetzen(100);
+          gameover.PositionSetzen(125, 250);
+          gameover.FarbeSetzen("rot");
+  
+          restart.TextSetzen("press r to restart");
+          restart.GanzNachVornBringen();
+          restart.SichtbarkeitSetzen(true);
+          restart.TextGrößeSetzen(50);
+          restart.PositionSetzen(200, 300);
+  
+          taktgeberStatus = false;
+          Zeichenfenster.TaktgeberStoppen();
+        }
       }
   }
 
