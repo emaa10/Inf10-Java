18ee8f1f6e3b370f63fa7c6bb8840769b10c5453
Emanuel Berger <emanuelberger49@gmail.com>
2024-06-10 08:38:51 +0200

    add apfel dings

---
+++ 9_7_2_2_A06_Vorl_Java_BlueJ_Bienen_Vogel-copy1/Berge.java
@@ -0,0 +1,43 @@
+
+/**
+ * Klasse zur Beschreibung eines Gebirges.
+ * 
+ * @author Klaus Reinold 
+ * @version 1.0
+ */
+class Berge extends Figur
+{
+    
+    /**
+     * Konstruktor der Berge
+     */
+    Berge()
+    {
+        super();
+        GrößeSetzen(200);
+        int x1 = -10;
+        int y1 = 50;
+        int x2 = 50;
+        int y2 = 50;
+        int x3 = 20;
+        int y3 = -20;
+        FigurteilFestlegenDreieck(x1, y1, x2, y2, x3, y3, "grau");
+        
+        x1 = -50;
+        y1 = 50;
+        x2 = 30;
+        y2 = 50;
+        x3 = -10;
+        y3 = -50;
+        FigurteilFestlegenDreieck(x1, y1, x2, y2, x3, y3, "grau");
+    }
+    
+    public void Bewegen(){
+        PositionSetzen(XPositionGeben()-5, YPositionGeben());
+        
+        if(XPositionGeben()<-100){
+            PositionSetzen(900, YPositionGeben());
+        }
+    }
+}
+    
