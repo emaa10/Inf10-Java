df26ad8573d1ebdb31922a289024853aa21c416c
Emanuel Berger <emanuelberger49@gmail.com>
2024-06-12 11:10:03 +0200

    add methods and attributes

--- 9_7_2_6_A04_Vorl_Java_BlueJ_Snake/Sonderfeld.java
+++ 9_7_2_6_A04_Vorl_Java_BlueJ_Snake/Sonderfeld.java
@@ -15,4 +15,16 @@ public class Sonderfeld {
         return symbol.YPositionGeben();
     }
 
+    public void Aktion() {
+    } // leer, da die Methode in den Unterklassen überschrieben werden muss
+
+    public void zufaelligPositionieren() {
+        int x;
+        int y;
+        do {
+            x = spiel.YKoordinateZufälligGeben();
+            y = spiel.XKoordinateZufälligGeben();
+        } while(!spiel.IstFrei(x, y));
+        symbol.PositionSetzen(x, y);
+    }
 }
